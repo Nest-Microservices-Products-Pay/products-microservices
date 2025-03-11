92d32b5ad9f93acb12802c770e82fabdae712045
Daniel Gomez <danigb66@gmail.com>
2025-03-11 16:17:42 -0500

    ts

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,6 @@ import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const logger = new Logger('Main');
-  console.log('aaaa', envs.natsServers)
   const app = await NestFactory.createMicroservice<MicroserviceOptions>(
     AppModule,
     {
